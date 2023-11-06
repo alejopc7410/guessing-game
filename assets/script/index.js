2a5b142842a5769f0b2f84b9b09a5662b3f138c3
Alejandro Pedraza <luisalejandropedrazacubillos@student.mitt.ca>
2023-11-06 17:14:11 -0600

    Reorganizing ideas

--- assets/script/index.js
+++ assets/script/index.js
@@ -11,52 +11,36 @@ function randomNumber () {
     return Math.floor(Math.random() * 25);
 }
 
+function validation (input) {
+    if (input.length > 0 && input.length <= 2) {
+        return true;
+    } else {
+        clue.innerText = 'Please, enter a 2-digit number';
+        return false;
+    }
+}
+
 let bigger = ["I'm more smaller", "You are close", "I'm not too bigger", "Subtract a little bit", "Not far away"]
 let smaller = ["I'm more bigger", "You are close", "I'm not too smaller", "Add a little bit", "Not far away"]
+let count = 5;
 
 function game() {
     let randomPhrase = Math.floor(Math.random() * 4);
     let value = input.value;
-    let count = 5; 
-
-    // for (let i = 5; i >= 0; i--) {
-    //     numberGuess.innerText = i;
-
-    //     if (value.length > 0 && value.length <= 2) {
-    //         if (value == random) {
-    //             clue.innerText = `Congratulations, the number is ${random}`;
-    //         } else if (value > random) {
-    //             count -= 1; // Actualizar count en cada rama del condicional
-    //             numberGuess.innerText = count;
-    //             clue.innerText = bigger[randomPhrase];
-    //         } else {
-    //             count -= 1; // Actualizar count en cada rama del condicional
-    //             numberGuess.innerText = count;
-    //             clue.innerText = smaller[randomPhrase];
-    //         }
-    //     } else {
-    //         clue.innerText = 'Please, enter a 2-digit number';
-    //         break;
-    //     }
-    // }
-
+     
     for (let i = 5; i >= 0; i--) {
-        numberGuess.innerText = i;
+        numberGuess.innerText = count;
 
-        if (value.length > 0 && value.length <= 2) {
-            return true;
-        } else {
-            clue.innerText = 'Please, enter a 2-digit number';
-        }
+        validation(value)
 
         if (value == random) {
             clue.innerText = `Congratulations, the number is ${random}`;
         } else if (value > random) {
-            count -= 1; // Actualizar count en cada rama del condicional
+            count -= 1; 
             numberGuess.innerText = count;
             clue.innerText = bigger[randomPhrase];
         } else {
-            count -= 1; // Actualizar count en cada rama del condicional
+            count -= 1; 
             numberGuess.innerText = count;
             clue.innerText = smaller[randomPhrase];
         }
