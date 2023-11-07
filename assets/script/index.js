87d05d5847fc4bb5f6bf98189bae8a63b59a29db
Alejandro Pedraza <luisalejandropedrazacubillos@student.mitt.ca>
2023-11-07 11:16:30 -0600

    Functional version of the game with clear parameter

--- assets/script/index.js
+++ assets/script/index.js
@@ -3,40 +3,43 @@
 const input = document.querySelector('.input');
 const clue = document.querySelector('.clue');
 const numberGuess = document.querySelector('.oportunity');
+const guessBtn = document.querySelector('.submit')
 let bigger = ["I'm more smaller", "You are close", "I'm not too bigger", "Subtract a little bit", "Not far away"]
 let smaller = ["I'm more bigger", "You are close", "I'm not too smaller", "Add a little bit", "Not far away"]
-let value = input.value.trim();
 let count = 5;
-let random = randomNumber()
 
 function randomNumber () {
     return Math.floor(Math.random() * 25);
 }
 
+let random = randomNumber()
+
 function checkNum (number) {
-    if (number.length > 2 || isNaN(number)) {
-        clue.innerText = 'Please, enter a valid two-digit number';
-        return;
+    const num = parseInt(number)
+
+    if (isNaN(num)) {
+        clue.innerText = 'Please enter a valid number';
+        return false;
+    } else if (num > 25) {
+        clue.innerText = 'Please enter a number smaller than 25';
+        return false;
+    } else {
+        clue.innerText = ''
+        return true;
     }
 }
 
-function game(value) {
+function game(number) {
     let randomPhrase = Math.floor(Math.random() * 4)
     
     if (count > 1) {
-        switch (true) {
-            case value === random:
-                clue.innerText = `Congratulations. The number is ${random}`
-                break;
-            case value < random:
-                clue.innerText = smaller[randomPhrase]
-                break;
-            case value > random:
-                clue.innerText = bigger[randomPhrase]
-                break;
-            case value > 25:
-                clue.innerText = "Type a number under 25"
-                break;
+        if (number == random) {
+            clue.innerText = `Congratulations. The number is ${random}`
+            guessBtn.style.display = 'none'
+        } else if (number < random) {
+            clue.innerText = smaller[randomPhrase]
+        } else {
+            clue.innerText = bigger[randomPhrase]
         }
     } else {
         clue.innerText = '¡¡GAME OVER!!';
@@ -44,9 +47,19 @@ function game(value) {
 }
 
 console.log(random)
-input.addEventListener('change', () => {
-    checkNum(value);
-    game(value);
-    count--;
-    numberGuess.innerText = `${count}`
+
+guessBtn.addEventListener('click', () => {
+    let value = input.value;
+    if (checkNum(value)) {
+        game(value);
+        count--;
+        numberGuess.innerText = `${count}`
+    }
+
+    if (count == 0) {
+        input.style.animation = 'flash'
+        input.style.animationDuration = '1s';
+        input.style.animationIterationCount = '3';
+        guessBtn.style.display = 'none'
+    }
 })
